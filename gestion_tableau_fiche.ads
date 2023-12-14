1d4dc6980ad54ef72541b014d23b76ff1ebeb911
Jazzjut <151069301+Jazzjut@users.noreply.github.com>
2023-12-14 13:53:40 +0100

    Update gestion_tableau_fiche.ads

--- gestion_tableau_fiche.ads
+++ gestion_tableau_fiche.ads
@@ -1,8 +1,7 @@
-WITH gestion_fiche;
-USE gestion_fiche;
+WITH gestion_fiche, outils;
+USE gestion_fiche, outils;
 
 PACKAGE Gestion_Tableau_Fiches IS
    TYPE T_TabFiche IS ARRAY (Integer RANGE 1..R) OF T_Fiche;
-   PROCEDURE visualisation_fiche(F : IN T_Fiche);
-   PROCEDURE Fiche_inscription_enfant(T : IN OUT T_TabFiche; S : IN OUT Gestion_Fiche.T_Sport);
+   PROCEDURE Fiche_inscription_enfant(T : IN OUT T_TabFiche; S : IN OUT Outils.T_Sport);
 END Gestion_Tableau_Fiches;
