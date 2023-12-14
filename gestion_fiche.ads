86d515cd6f02c591c66389afca2f0199f53c7a66
Jazzjut <151069301+Jazzjut@users.noreply.github.com>
2023-12-14 13:52:52 +0100

    Update gestion_fiche.ads

--- gestion_fiche.ads
+++ gestion_fiche.ads
@@ -5,17 +5,17 @@ PACKAGE Gestion_Fiche IS
    TYPE T_Fiche IS RECORD
       Nom : T_Mot := (others => ' ');
       Prenom : T_Mot := (others => ' ');
-      Sexe : Boolean := false; -- false = gar�on, true = fille
+      Sexe : Boolean := false; -- false = garçon, true = fille
       Age : Positive := 4;
       NbFS : Natural := 0;
       PxH : Float := 0.0;
       SP : T_TabSport;
       Vide : boolean := true; -- si vide = true, la case est vide
    END RECORD;
-   M : CONSTANT Integer := 5;
-   R : Natural;-- range
-    --R := M*T_Sport'RANGE; à mettre dans l'adb
 -- inscription d'un nouvel enfant
    PROCEDURE Inscription (F : IN OUT T_Fiche);
+   PROCEDURE AffichageSP (SP : IN T_TabSport);
+   --PROCEDURE Visualisation_Fiche(F : IN T_Fiche);
+   FUNCTION Calcul_Prix (F : T_Fiche) RETURN Float;
+   
 END Gestion_Fiche;
-
