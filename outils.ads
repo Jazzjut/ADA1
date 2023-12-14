25e21804303644db3ef3a6160b0585f95bad22ef
Jazzjut <151069301+Jazzjut@users.noreply.github.com>
2023-12-14 13:54:33 +0100

    Update outils.ads

--- outils.ads
+++ outils.ads
@@ -3,15 +3,10 @@ PACKAGE Outils IS
    TYPE T_Sport IS (Judo,Tennis,Natation,Football,Danse,Rugby);
    TYPE T_TabSport IS ARRAY (T_Sport) OF Boolean;
    SUBTYPE T_Mot IS String(1..50);
+   M : CONSTANT Integer := 5;
+   R : Natural;-- range
+    --R := M*T_Sport'RANGE;
    PROCEDURE Saisie (Mot : OUT T_Mot);
    PROCEDURE Saisie_Ch (Ch : OUT Natural);
-   PROCEDURE AffichageSP (SP : IN T_TabSport);
-   FUNCTION Reduc (Nbfs : IN Integer; PxH : IN Float) RETURN Float;
 
 END Outils;
-
-
-
-
-
-
